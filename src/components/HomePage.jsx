fd88b2bb297f6eb22982e74dab816268abad1ea3
aniketsriwastva <aniket.sriwastva@gmail.com>
2023-09-29 00:25:10 +0530

    tailwind setup

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="text-3xl font-bold underline text-ellipsis">HomePage</div>
+  );
 }
 
 export default HomePage;
