b09653386e6d1d713ba34c2c0fa2d8f5b861da7c
aniketsriwastva <aniket.sriwastva@gmail.com>
2023-10-17 23:09:20 +0530

    enhanced personal profile modal

--- src/components/PersonalProfileModal.js
+++ src/components/PersonalProfileModal.js
@@ -31,9 +31,25 @@ const PersonalProfileModal = () => {
   return (
     <div>
       <CentreModal>
+        <div className="block">
+          <div>
+            <label className="font-bold">Full Name</label>
+            <div className="flex">
+              <div className="w-full">
+                <input
+                  type="text"
+                  // value={name}
+                  // onChange={(e) => setName(e.target.value)}
+                  className="border p-2 pl-4 w-full flex mb-4 mt-1 border-black rounded-2xl"
+                  placeholder="Enter Your Name"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="block mb-3">
           <div>
-            <label>Gender</label>
+            <label className="font-bold">Gender</label>
           </div>
           <div className="gap-3 flex mt-1">
             <span className="p-2 border rounded-2xl border-gray-400">Male</span>
@@ -45,9 +61,9 @@ const PersonalProfileModal = () => {
             </span>
           </div>
         </div>
-        <div className="block">
+        <div className="block mb-3">
           <div>
-            <label>Date of birth</label>
+            <label className="font-bold">Date of birth</label>
           </div>
           <div className="gap-3 flex mt-1">
             <span>
@@ -81,6 +97,32 @@ const PersonalProfileModal = () => {
               />
             </span>
           </div>
+        </div>
+        <div className="block mb-3">
+          <div>
+            <label className="font-bold">Have you taken a career break</label>
+            <div className="flex gap-2">
+              <input type="radio" name="yes" value="Yes" />
+              <label className="mr-32">Yes</label>
+              <input type="radio" name="no" value="No" />
+              <label>No</label>
+            </div>
+          </div>
+          <div className="gap-3 flex mt-1"></div>
+        </div>
+
+        <div className="block mb-3">
+          <div>
+            <label className="font-bold">Address</label>
+            <input
+              type="text"
+              // value={name}
+              // onChange={(e) => setName(e.target.value)}
+              className="border p-2 pl-4 w-full flex mb-4 mt-1 border-black rounded-2xl"
+              placeholder="Enter Your Address"
+            />
+          </div>
+          <div className="gap-3 flex mt-1"></div>
         </div>
       </CentreModal>
     </div>
